80ed0878dea556f7d3bb83d0df2d1530d3e43363
shymine <timothee.neitthoffer@laposte.net>
2024-07-26 15:30:34 +0200

    Update slice-utils.go
    changing map so that there are less initialisations
    adding Intersection and Contains functions

--- slice-utils.go
+++ slice-utils.go
@@ -1,9 +1,9 @@
 package collectionutils
 
 func Map[A any, B any](list []A, f func(A) B) []B {
-	res := []B{}
-	for _, el := range list {
-		res = append(res, f(el))
+	res := make([]B, len(list))
+	for i, el := range list {
+		res[i] = f(el)
 	}
 	return res
 }
@@ -24,4 +24,23 @@ func Filter[A any](list []A, f func(A) bool) []A {
 		}
 	}
 	return res
+}
+
+func Intersection[A any](a []A, b []A) []A {
+	res := []A{}
+	for _, el := range a {
+		if Contains(b, el) {
+			res = append(res, el)
+		}
+	}
+	return res
+}
+
+func Contains[A any](list []A, elem A) bool {
+	for _, el := range list {
+		if el == elem {
+			return true
+		}
+	}
+	return false
 }
